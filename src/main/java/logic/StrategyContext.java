c68e601e8f4c08b95349894cb68c6b205dbab8f2
Ainur Makhmet <ainur.makhmet@gmail.com>
2017-03-12 17:45:12 +0000

    adds logging lines and introduce one common logger in a class that takes the instance of a logger depending on the chosen strategy.

--- src/main/java/logic/StrategyContext.java
+++ src/main/java/logic/StrategyContext.java
@@ -1,8 +1,11 @@
 package logic;
 
+import javafx.util.Pair;
 import models.DatabaseEntity;
 import models.Project;
+import models.SystemData;
 import models.Task;
+import org.apache.logging.log4j.Logger;
 import servers.LocalServer;
 
 import java.io.IOException;
@@ -26,14 +29,19 @@ public class StrategyContext {
             projectResultList;
     private int numberOfTasksUnvalidForAllocation,
             numberOfUnnalocatedTasks,
-            numberOfUnnalocatedProjects,
-            numberOfUnnalocatedTasksOfHighPriority,
-            numberOfUnnalocatedTasksOfMediumPriority,
-            numberOfUnnalocatedTasksOfLowPriority;
+            numberOfUnnalocatedProjects;
     private Strategy strategy;
+    private Logger logger;
 
     public StrategyContext(Strategy allocationAlgorithm) {
         strategy = allocationAlgorithm;
+        if (strategy.getClass().equals(MaximumProfitAlgorithm.class)) {
+            logger = LocalServer.mpLogger;
+        } else if (strategy.getClass().equals(FordFulkersonAlgorithm.class)) {
+            logger = LocalServer.ffLogger;
+        } else if (strategy.getClass().equals(GreedyAlgorithm.class)) {
+            logger = LocalServer.gLogger;
+        }
     }
 
     public List<Task> executeTaskStrategy(List<Task> tasksToAllocate){
@@ -45,32 +53,40 @@ public class StrategyContext {
         //TODO: fix Greedy returns only allocated tasks. Need to return all tasks.
         for (int i = 0; i < numTries; ++i) {
             if (!highPriorityTasks.isEmpty()) {
-                result.addAll(strategy.allocate(highPriorityTasks));
-                numberOfUnnalocatedTasksOfHighPriority +=  strategy.numOfUnnalocatedTasks;
+                allocateTasks(highPriorityTasks, "high");
             }
             if (!mediumPriorityTasks.isEmpty()) {
-                result.addAll(strategy.allocate(mediumPriorityTasks));
-                numberOfUnnalocatedTasksOfMediumPriority +=  strategy.numOfUnnalocatedTasks;
+                allocateTasks(mediumPriorityTasks, "medium");
             }
             if (!lowPriorityTasks.isEmpty()) {
-                result.addAll(strategy.allocate(lowPriorityTasks));
-                numberOfUnnalocatedTasksOfLowPriority +=  strategy.numOfUnnalocatedTasks;
+                allocateTasks(lowPriorityTasks, "low");
             }
-
         }
         long endTime = System.currentTimeMillis();
-        LocalServer.iLogger.info(strategy.getClass().getSimpleName()+": Total time for {} tries: {} ms", numTries, (endTime-begTime));
-        numberOfUnnalocatedTasks+=numberOfTasksUnvalidForAllocation
-                +numberOfUnnalocatedTasksOfHighPriority
-                +numberOfUnnalocatedTasksOfMediumPriority
-                +numberOfUnnalocatedTasksOfLowPriority;
+        logger.info(strategy.getClass().getSimpleName()+": Total time for {} tries: {} ms", numTries, (endTime-begTime));
+        logger.info("Total number of unallocated tasks: {}. Among them number of tasks non-valid for allocation: {}\n",numberOfUnnalocatedTasks, numberOfTasksUnvalidForAllocation);
+
         result.sort(new EntityComparator());
-        result.forEach(task -> LocalServer.iLogger.info(task.toString()));
-        LocalServer.iLogger.info("Total number of unallocated tasks: {}. Among them number of tasks non-valid for allocation: {}\n",numberOfUnnalocatedTasks, numberOfTasksUnvalidForAllocation);
+        result.forEach(task -> logger.info(task.toString()));
         return result;
     }
+
     /**
-     * distribute tasks of different priority into separate list to deal with them separately
+     *
+     * @param listOfTasks tlists of tasks to Allocate. All the tasks in the list are of one priority level.
+     * @param priority priority level for logging purposes.
+     */
+    private void allocateTasks(List<Task> listOfTasks, String priority) {
+        if (!listOfTasks.isEmpty()) {
+            logger.info("Start allocating the list of {} priority tasks with a size of {}", priority, listOfTasks.size());
+            result.addAll(strategy.allocate(listOfTasks));
+            numberOfUnnalocatedTasks +=  strategy.numOfUnnalocatedTasks;
+        }
+    }
+
+    /**
+     * distribute tasks of different priority into separate list to deal with them separately.
+     * Is used internally by executeTaskStrategy.
      */
     private void distributeValidTasksForAllocationByPriority() {
         numberOfUnnalocatedTasks = 0;
@@ -105,34 +121,30 @@ public class StrategyContext {
 
     }
 
-    static class EntityComparator implements Comparator<DatabaseEntity> {
-        @Override
-        public int compare(DatabaseEntity e1, DatabaseEntity e2) {
-            if (e1.getId() > e2.getId()) return 1;
-            else if (e1.getId() == e2.getId()) return 0;
-            else return -1;
-        }
-    }
-
     public List<Project> executeProjectStrategy(List<Project> projectsToAllocate){
         numberOfUnnalocatedProjects=0;
         int totalProfitFromSelectedProjects = 0;
         projectResultList = new ArrayList<>();
+        getTasksInfo();
+
         Iterator<Project> iter = projectsToAllocate.iterator();
         while (iter.hasNext()) {
             Project project = iter.next();
             try {
                 project.getTasks().forEach(t -> {
+                    Task task = SystemData.getAllTasksMap().get(t.getId());
+                    task.setRecommendedAssignee(null);
                     if (t.getStartTime() == null || t.getEndTime() == null) {
                         projectResultList.add(project);
                         iter.remove();
-                        LocalServer.mpLogger.info("project contains with unspecified start tate or end date, therefore will not be considered for allocation");
+                        logger.info("project contains with unspecified start tate or end date, therefore will not be considered for allocation");
                     }
                 });
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
         int numTries = 1;
         long begTime = System.currentTimeMillis();
         for (int i = 0; i < numTries; ++i) {
@@ -149,12 +161,52 @@ public class StrategyContext {
             }
         }
         long endTime = System.currentTimeMillis();
-        LocalServer.iLogger.info(strategy.getClass().getSimpleName()+": Total time for {} tries: {} ms", numTries, (endTime-begTime));
+        logger.info(strategy.getClass().getSimpleName()+": Total time for {} tries: {} ms", numTries, (endTime-begTime));
+        logger.info("Total number of unallocated projects: {}.\n",numberOfUnnalocatedProjects);
+        logger.info(strategy.getClass().getSimpleName()+": Max profit = {}", totalProfitFromSelectedProjects);
 
         projectsToAllocate.sort(new EntityComparator());
-
-        LocalServer.iLogger.info("Total number of unallocated projects: {}.\n",numberOfUnnalocatedProjects);
-        LocalServer.iLogger.info(strategy.getClass().getSimpleName()+": Max profit = {}", totalProfitFromSelectedProjects);
         return projectsToAllocate;
+    }
+
+    /**
+     * the method is used only for debugging purposes by executeProjectStrategy method.
+     */
+    private void getTasksInfo() {
+        Set<Pair<Task, Task>> taskPairs = new HashSet<>();
+        for (Project project : projectsToAllocate) {
+            logger.info(project.getName().toString());
+            try {
+                for (Task task1: project.getTasks()) {
+                    project.getTasks().forEach(task2 -> {
+                        if (!task1.equals(task2)) {
+                            Pair pair = new Pair(task1, task2);
+                            if (!(taskPairs.contains(pair) || taskPairs.contains(new Pair(task2, task1))))
+                                taskPairs.add(new Pair(task1, task2));
+                        }
+                    });
+                    logger.info(task1.toString());
+                }
+                for (Pair<Task, Task> taskPair: taskPairs) {
+                    Task task0 = taskPair.getKey();
+                    Task task1 = taskPair.getValue();
+                    if (task0.timeOverlapWith(task1)) {
+                        logger.info("Task {} has timeOverllaping with Task {}", task0.getId(), task1.getId());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    static class EntityComparator implements Comparator<DatabaseEntity> {
+        @Override
+        public int compare(DatabaseEntity e1, DatabaseEntity e2) {
+            if (e1.getId() > e2.getId()) return 1;
+            else if (e1.getId() == e2.getId()) return 0;
+            else return -1;
+        }
     }
 }
